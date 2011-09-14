20195f7379a01ca795f492fca56642ab28f02444
Jonatan J?nsson <jonteJJ@gmail.com>
2011-09-15 00:11:55 +0200

    Fixed refactoring issue.

--- maintenance.php
+++ maintenance.php
@@ -4,6 +4,14 @@ require_once 'includes.php';
 
 $dbh = new DatabaseHandler();
 
+$movie = $dbh->getProduction(5275);
+
+
+MovieMiner::getBasicInfo($movie);
+echo $movie->toString();
+die();
+
+
 $objects = $dbh->listProductions("production.id", "DESC", 1, IGNORED_RSSMOVIE); //"production.rating", "=", 0, false);
 
 foreach($objects as $movie)
